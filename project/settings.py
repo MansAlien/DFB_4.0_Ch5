a9bc4fc8f4c9b28478b316bdf6315ce05b36e06a
MansAlien <mans012033945421@gmail.com>
2024-01-08 20:06:23 +0200

    vercel

--- project/settings.py
+++ project/settings.py
@@ -129,12 +129,12 @@ STATIC_URL = 'static/'
 # STATICFILES_DIRS = [BASE_DIR/'static',]
 # STATIC_ROOT = BASE_DIR/'staticfiles'
 
-STATICFILES_DIRS = [BASE_DIR/ 'static']
+# STATICFILES_DIRS = [BASE_DIR/ 'static']
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 
 # Default primary key field type
